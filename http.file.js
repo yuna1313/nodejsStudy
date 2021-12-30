260ef7d1ee319db79649907cc485109c4b4b9aba
yuna1313 <93983333+yuna1313@users.noreply.github.com>
2021-12-30 22:51:36 +0900

    Part3 Node.js를 사용한 웹 개발
    Chapter6 http 모듈
    HTML 페이지 이미지 및 오디오 출력

--- http.file.js
+++ http.file.js
@@ -9,6 +9,7 @@ require('http').createServer(function (request, response) {
 });
 */
 
+/*
 // 모듈을 추출합니다.
 var fs = require('fs');
 var http = require('http');
@@ -22,4 +23,31 @@ http.createServer(function(request, response) {
     });
 }).listen(52273, function() {
     console.log('Server Running at http://127.0.0.1:52273');
+});
+*/
+
+// 모듈을 추출합니다.
+var fs = require('fs');
+var http = require('http');
+
+// 52273번 포트에 서버를 생성하고 실행합니다.
+http.createServer(function(request, response) {
+    // 이미지 파일을 읽습니다.
+    fs.readFile('logo.jpg', function(error, data) {
+        response.writeHead(200, {'Content-Type' : 'image/jpeg'});
+        response.end(data);
+    });
+}).listen(52273, function() {
+    console.log('Server Running at http://127.0.0.1:52273');
+});
+
+// 52274번 포트에 서버를 생성하고 실행합니다.
+http.createServer(function(request, response) {
+    // 음악 파일을 읽습니다.
+    fs.readFile('Floating Home.mp3', function(error, data) {
+        response.writeHead(200, {'Content-Type' : 'audio/mp3'});
+        response.end(data);
+    })
+}).listen(52274, function() {
+    console.log('Server Running at http://127.0.0.1:52274');
 });
