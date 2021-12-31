7831338c11185c045526d0bb46c5ed45ad826f03
yuna1313 <93983333+yuna1313@users.noreply.github.com>
2021-12-31 20:53:43 +0900

    Chapter6 http 모듈
    쿠키 생성 및 request 객체

---
+++ http.page.js
@@ -0,0 +1,12 @@
+// 모듈을 추출합니다.
+var http = require('http');
+
+// 서버를 생성하고 실행합니다.
+http.createServer(function(request, response) {
+    var date = new Date();
+    date.setDate(date.getDate() + 7);
+    response.writeHead(302, { 'Location' : 'http://www.hanbit.co.kr' });
+    response.end();
+}).listen(52273, function() {
+    console.log('Server Running at http://127.0.0.1:52273');
+});
