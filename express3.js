13ffc33a2d7016903c1c2a97f7e591f3d496690f
yuna1313 <93983333+yuna1313@users.noreply.github.com>
2022-01-03 20:56:15 +0900

    Chapter8 express 모듈
    기본 요청 메소드

---
+++ express3.js
@@ -0,0 +1,21 @@
+// 모듈을 추출합니다.
+var express = require('express');
+
+// 서버를 생성합니다.
+var app = express();
+
+// 미들웨어를 설정합니다.
+app.use(function(request, response, next) {
+    // User-Agent 속성을 추출합니다.
+    var agent = request.header('User-Agent');
+    console.log(request.headers);
+    console.log(agent);
+    // 응답합니다.
+    // 상태 코드만 보낼 때는 sendStatus() 메서드를 사용합니다.
+    response.sendStatus(200);
+});
+
+// 서버를 실행합니다.
+app.listen(52273, function() {
+    console.log('Server Running at http://127.0.0.1:52273');
+})
