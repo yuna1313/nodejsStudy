eac340c566d6619138b1f0ecdf9aebe5a7b776b1
yuna1313 <93983333+yuna1313@users.noreply.github.com>
2022-01-05 20:58:19 +0900

    Chapter8 express 모듈
    cookie parser 미들웨어

---
+++ express11.js
@@ -0,0 +1,16 @@
+// 모듈을 추출합니다.
+var express = require('express');
+var morgan = require('morgan');
+
+// 서버를 생성합니다.
+var app = express();
+
+app.use(morgan('combined'));
+app.use(function(request, response) {
+   response.send('<h1>express Basic</h1>');
+});
+
+// 서버를 실행합니다.
+app.listen(52273, function() {
+    console.log('Server Running at http://127.0.0.1:52273');
+})
---
+++ express12.js
@@ -0,0 +1,17 @@
+// 모듈을 추출합니다.
+var express = require('express');
+var morgan = require('morgan');
+
+// 서버를 생성합니다.
+var app = express();
+
+// 미들웨어를 설정합니다.
+app.use(morgan(':method + :date'));
+app.use(function(request, response) {
+   response.send('<h1>express Basic</h1>');
+});
+
+// 서버를 실행합니다.
+app.listen(52273, function() {
+    console.log('Server Running at http://127.0.0.1:52273');
+})
